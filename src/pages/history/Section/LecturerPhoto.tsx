bf9207dfd2cec894e364e1cce3a5659ccf5de754
Kobzar <kobzar_stanislav@knu.ua>
2024-04-01 20:37:50 +0300

    photos are cursed

--- src/pages/history/Section/LecturerPhoto.tsx
+++ src/pages/history/Section/LecturerPhoto.tsx
@@ -1,11 +1,11 @@
 export const LecturerPhoto = (props: {src: string}) => {
     return <div style={{
         backgroundImage: `url(${props.src})`,
+        backgroundPosition: 'left center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '100% 100%',
         width: '275px',
         height: '388px',
         borderRadius: '12px',
-        position: 'relative',
-        top: -20,
-        left: -20
     }}></div>
 }
