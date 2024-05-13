7b1c2bdce13b0f682695ae0def081f932bba3f81
Kobzar <kobzar_stanislav@knu.ua>
2024-05-13 19:50:15 +0300

    study main page

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ type Props = {
     children: ReactNode
 }
 export const Layout = ({children}: Props) => {
-    return <div className="layout px-36 flex flex-col gap-[5.5rem] my-12">
+    return <div className="layout px-36 flex flex-col gap-[4rem] my-12">
         {children}
     </div>
 }
