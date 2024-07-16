fc63deaf50f77f734177d9c76a63627aeb5b27c6
Kobzar Stanislav <kobzar_stanislav@knu.ua>
2024-07-16 19:35:00 +0300

    failed atempt at animation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,18 @@
 import { CathedraLogo } from '../common/cathedra-logo/CathedraLogo'
-import { Button, Group, Menu } from '@mantine/core';
+import { Button, Menu } from '@mantine/core';
 import { navbar } from './navbar';
+import { MenuIcon, XIcon } from 'lucide-react';
+import { LinkPNG } from '../common/RedirectLink/LinkPNG';
+import { useState } from 'react';
 
 export const Header = () => {
+    const [open, setOpen] = useState(false)
     return (
-        <div className='flex justify-center items-center gap-16 px-36 h-20 w-full relative left-0 top-0' style={{
+        <div className='flex gap-16 px-12 h-20 w-screen relative left-0 top-0 justify-between md:justify-center items-center md:px-36' style={{
             boxShadow: "0px 4px 77px -26px rgba(0,0,0,0.25)",
         }}>
             <a href='/'><CathedraLogo height={35} width={105}/></a>
-            <Group>
+            <div className='hidden md:flex h-full text-center items-center flex-nowrap'>
                 {
                     navbar.map(nav => <Menu loop={false} trigger='hover' withinPortal={false}
                     trapFocus={false}>
@@ -23,7 +27,27 @@ export const Header = () => {
                         }
                     </Menu>)
                 }
-            </Group>
+            </div>
+            <div className='md:hidden'>
+                <MenuIcon className='' onClick={() => setTimeout(() => setOpen(true), 300)}/>
+                <div className={!open ? 'hidden' : ''}>
+                    <div className='w-screen h-screen bg-gray-500 opacity-40 z-10 absolute top-0 right-0'></div>
+                    <div className='bg-white p-10 w-3/4 h-screen absolute top-0 z-20' style={{
+                            right: open ? '0' : '-100%',
+                            transitionProperty: 'right',
+                            transitionDuration: '10s',
+                    }}>
+                        <XIcon className='relative top-0 left-0' onClick={() => setOpen(false)}/>
+                        <div className='mt-6 mx-2 text-2xl flex flex-col gap-4 justify-start items-start'>
+                        {
+                            navbar.map(nav => <button>
+                                <span><LinkPNG size='25'/> {nav.target}</span>
+                            </button>)
+                        }
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
