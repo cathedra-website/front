8d1a9e34845139fa27a98637d80db79d216b474d
Andrii Semenystyi <46621668+MeowningMaster@users.noreply.github.com>
2024-08-18 18:57:29 +0300

    save

--- src/pages/Home/sections/Welcome.tsx
+++ src/pages/Home/sections/Welcome.tsx
@@ -6,7 +6,7 @@ export const Welcome = () => {
     const navigate = useNavigate()
     return <div>
         <div>
-            <h3 className="font-light tracking-widest text-[#1E1E1E80] text-xl">Київський національний університет імені Тараса Шевченка</h3>
+            <h3 className="tracking-widest text-[#1E1E1E80] text-xl">Київський національний університет імені Тараса Шевченка</h3>
             <h1 className="font-bold text-7xl leading-tight mb-4">Кафедра інтелектуальних програмних систем</h1>
             <h2 className="font-medium text-[28px] text-[#606B70]">Факультет комп’ютерних наук та кібернетики</h2>
         </div>
@@ -16,7 +16,7 @@ export const Welcome = () => {
                 onClick={() => navigate({to: '/cathedra'})}>
                     Дізнатись більше
             </Button>
-            <CathedraLogo height={145} width={420}/>
+            {/* <CathedraLogo height={145} width={420}/> */}
         </div>
     </div>
 }
